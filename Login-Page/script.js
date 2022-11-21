98d00f7cc0a84e0b2c4c3a3ba4d8a2f7656d6ed2
Edvin Ekström <ekstrom.edvin@gmail.com>
2022-11-21 13:46:50 +0100

    test 2

--- Login-Page/script.js
+++ Login-Page/script.js
@@ -1,8 +1,21 @@
+const username = document.getElementById("username")
+const usernameBtn = document.getElementById("usernameBtn")
+const password = document.getElementById("password")
+const passwordBtn = document.getElementById("passwordBtn")
 
+if (localStorage.getItem("personsRegister")) {
+    console.log("Det finns sparat i LS");
+} else {
+    console.log("Finns inget sparat i LS");
 
+    let personsRegister = [
+        {username:"janne", password:"test"},
+        {username:"edvin", password:"test"},
+        {username:"test", password:"3"},
+    ];
 
-let persons = [
-    {username: "janne", password: "test"},
-    {username: "edvin", password: "test"},
-    {username: "test", password: "3"},
-];
+    localStorage.setItem("personsRegister", JSON.stringify(personsRegister));
+}
+
+/*Om besökaren ej är inloggad så skall ett inloggningsformulär visas.
+Är besökaren inloggad så skall istället en logga-ut knapp visas. */
