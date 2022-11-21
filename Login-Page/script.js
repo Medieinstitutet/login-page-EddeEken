71c71b6487a7a2401c03eddca29e793c4f14eaa2
Edvin Ekström <ekstrom.edvin@gmail.com>
2022-11-21 18:44:47 +0100

    skiss 1

--- Login-Page/script.js
+++ Login-Page/script.js
@@ -3,6 +3,8 @@ const usernameBtn = document.getElementById("usernameBtn")
 const password = document.getElementById("password")
 const passwordBtn = document.getElementById("passwordBtn")
 
+// fånga och skriv ut värdena från de två inputen i LS
+
 if (localStorage.getItem("personsRegister")) {
     console.log("Det finns sparat i LS");
 } else {
@@ -17,5 +19,11 @@ if (localStorage.getItem("personsRegister")) {
     localStorage.setItem("personsRegister", JSON.stringify(personsRegister));
 }
 
+//lyckat login 
+
+//misslyckat login
+
 /*Om besökaren ej är inloggad så skall ett inloggningsformulär visas.
 Är besökaren inloggad så skall istället en logga-ut knapp visas. */
+
+// byt ut inloggningsformulären till en logga ut knapp när man har loggat in 
